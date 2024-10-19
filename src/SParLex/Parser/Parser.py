5f8398fd125be8ef495d3b98841e08d051a44601
Sam Gardner <samuelgardner101@gmail.com>
2024-10-20 00:19:48 +0100

    Configurability for auto-newlines

--- src/SParLex/Parser/Parser.py
+++ src/SParLex/Parser/Parser.py
@@ -96,7 +96,7 @@ class Parser(ABC):
 
         c1 = self.current_pos()
 
-        while self._auto_new_line and token_type != self._token_set.newline_token() and self.current_tok().token_type in self._auto_skip:
+        while token_type != self._token_set.newline_token() and self.current_tok().token_type in self._auto_skip:
             self._index += 1
         while token_type == self._token_set.newline_token() and self.current_tok().token_type == self._token_set.whitespace_token():
             self._index += 1
